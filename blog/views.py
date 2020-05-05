4b2ed236ea245ff3c88bf46c978304ac8c0879f1
Chandan Panigrahi <cpanigrahiofficial99@gmail.com>
2020-05-06 02:14:35 +0530

    user accounts set up

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+from django.urls import reverse, reverse_lazy
 
 
 class BlogListView(ListView):
@@ -10,3 +12,27 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')    # second way of defining success URL
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+    def get_success_url(self):
+        return reverse('post_detail', args=[str(self.object.pk)])
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+    def get_success_url(self):
+        return reverse('post_detail', args=[str(self.object.pk)])
