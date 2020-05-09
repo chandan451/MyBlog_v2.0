218f796fc2fce14ed1ed0da66c1f0b9eaff1037f
Chandan Panigrahi <cpanigrahiofficial99@gmail.com>
2020-05-10 02:11:24 +0530

    final commit after user model and access setup

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
 from django.urls import reverse, reverse_lazy
@@ -14,13 +15,13 @@ class BlogDetailView(DetailView):
     template_name = 'post_detail.html'
 
 
-class BlogDeleteView(DeleteView):
+class BlogDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('home')    # second way of defining success URL
 
 
-class BlogUpdateView(UpdateView):
+class BlogUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
@@ -29,10 +30,14 @@ class BlogUpdateView(UpdateView):
         return reverse('post_detail', args=[str(self.object.pk)])
 
 
-class BlogCreateView(CreateView):
+class BlogCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_new.html'
-    fields = ['title', 'author', 'body']
+    fields = ['title', 'body']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
     def get_success_url(self):
         return reverse('post_detail', args=[str(self.object.pk)])
